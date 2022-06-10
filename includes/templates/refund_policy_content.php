690314b6bee378b40d9aa77f5a0be10fbbea325e
Megha Sunny <meghasunnym@gmail.com>
2022-06-10 13:09:54 +0530

    Privacy & Refund policy, terms & conditions fix

--- includes/templates/refund_policy_content.php
+++ includes/templates/refund_policy_content.php
@@ -4,20 +4,45 @@
             <div class="col-lg-12">
                 <h4 data-aos="fade-up" data-aos-delay="200">
                     <p>
-                        All sales of aluminium fabrication on the Naiz Platform are final with no refund or exchange
-                        permitted.This includes the product amount and the associated service charges including the
-                        applicable taxes The user is responsible for his/her User id for which the user use to purchase
-                        the product and all charges that result from those purchases. We are not responsible for any
-                        purchase of aluminium fabrication for an incorrect User ID) However, if in a transaction
-                        performed by the user on the Naiz Platform, money has been charged to user card or bank account
-                        and the product is not delivered within 72 hours of the completion of the transaction, then user
-                        shall inform us by sending an email to our customer services email address mentioned on the
-                        support page on or at the support chat option provided in the application. Please include in the
-                        e-mail the following details Product Id, Product Name, Transaction date and Transaction id. We
-                        will investigate the incident and, if it is found that money was indeed charged to the user card
-                        or bank account without delivery of the product, then the user account will be credited the
-                        money within 7-10 working days from the date of receipt of Your email. For the payment failure
-                        issues due to error in payment gateway, the payment gateway policy will be applicable
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000"><strong>Returns</strong></span></span></span></p>
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000">Our policy lasts 30 days. If 30 days have gone by since your purchase, unfortunately we can&rsquo;t offer you a refund or exchange.</span></span></span></p>
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000">To be eligible for a return, your item must be unused and in the same condition that you received it. It must also be in the original packaging.</span></span></span></p>
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000">Several types of goods are exempt from being returned. Perishable goods such as food, flowers, newspapers or magazines cannot be returned. We also do not accept products that are intimate or sanitary goods, hazardous materials, or flammable liquids or gases.</span></span></span></p>
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000">Additional non-returnable items:</span></span></span></p>
+                    <p>
+                    <ul style="margin-left: 40px">
+                        <li style="list-style-type:disc"><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000">Gift cards</span></span></span></li>
+                        <li style="list-style-type:disc"><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000">Downloadable software products</span></span></span></li>
+                        <li style="list-style-type:disc"><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000">Some health and personal care items</span></span></span></li>
+                    </ul>
+                    </p>
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000">To complete your return, we require a receipt or proof of purchase.</span></span></span></p>
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000">Please do not send your purchase back to the manufacturer.</span></span></span></p>
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000">There are certain situations where only partial refunds are granted: (if applicable)</span></span></span></p>
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000">Book with obvious signs of use</span></span></span></p>
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000">CD, DVD, VHS tape, software, video game, cassette tape, or vinyl record that has been opened.</span></span></span></p>
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000">Any item not in its original condition, is damaged or missing parts for reasons not due to our error.</span></span></span></p>
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000">Any item that is returned more than 30 days after delivery</span></span></span></p>
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000"><strong>Refunds (if applicable)</strong></span></span></span></p>
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000">Once your return is received and inspected, we will send you an email to notify you that we have received your returned item. We will also notify you of the approval or rejection of your refund.</span></span></span></p>
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000">If you are approved, then your refund will be processed, and a credit will automatically be applied to your credit card or original method of payment, within a certain amount of days.&nbsp;</span></span></span></p>
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000"><strong>Late or missing refunds (if applicable)</strong></span></span></span></p>
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000">If you haven&rsquo;t received a refund yet, first check your bank account again.</span></span></span></p>
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000">Then contact your credit card company, it may take some time before your refund is officially posted.</span></span></span></p>
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000">Next contact your bank. There is often some processing time before a refund is posted.</span></span></span></p>
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000">If you&rsquo;ve done all of this and you still have not received your refund yet, please contact us at </span></span></span><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000"><strong>naiztrading2021@gmail.com</strong></span></span></span><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000">.</span></span></span></p>
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000"><strong>Sale items (if applicable)</strong></span></span></span></p>
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000">Only regular priced items may be refunded, unfortunately sale items cannot be refunded.</span></span></span></p>
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000"><strong>Exchanges (if applicable)</strong></span></span></span></p>
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000">We only replace items if they are defective or damaged.&nbsp; If you need to exchange it for the same item, send us an email at </span></span></span><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000"><strong>naiztrading2021@gmail.com </strong></span></span></span><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000">and send your item to: 622 Manglam Electronic Market Jaipur Rajasthan India 302001.</span></span></span></p>
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000"><strong>Gifts</strong></span></span></span></p>
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000">If the item was marked as a gift when purchased and shipped directly to you, you&rsquo;ll receive a gift credit for the value of your return. Once the returned item is received, a gift certificate will be mailed to you.</span></span></span></p>
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000">If the item wasn&rsquo;t marked as a gift when purchased, or the gift giver had the order shipped to themselves to give to you later, we will send a refund to the gift giver and he will find out about your return.</span></span></span></p>
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000"><strong>Shipping</strong></span></span></span></p>
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000">To return your product, you should mail your product to: 622 Manglam Electronic Market Jaipur Rajasthan India 302001.</span></span></span></p>
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000">You will be responsible for paying for your own shipping costs for returning your item. Shipping costs are non-refundable. If you receive a refund, the cost of return shipping will be deducted from your refund.</span></span></span></p>
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000">Depending on where you live, the time it may take for your exchanged product to reach you, may vary.</span></span></span></p>
+                    <p><span style="font-size:11pt"><span style="font-family:Arial"><span style="color:#000000">If you are shipping an item over $75, you should consider using a trackable shipping service or purchasing shipping insurance. We don&rsquo;t guarantee that we will receive your returned item.</span></span></span></p>
                     </p>
                 </h4>
             </div>
